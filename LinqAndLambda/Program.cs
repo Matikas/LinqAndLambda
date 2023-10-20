0ef6e0c4d7b616c875e5836b43588567caf4e901
Marius Paškevičius <marius.paskevicius@yahoo.com>
2023-10-20 20:37:32 +0300

    linq

--- LinqAndLambda/Program.cs
+++ LinqAndLambda/Program.cs
@@ -4,45 +4,75 @@
     {
         static void Main(string[] args)
         {
-            Func<int, int> multiplyByFive = (int num) =>
-            {
-                Console.WriteLine("tuoj dauginsiu is 5");
-                int result = num * 5;
-                return result;
-            };
-            int result = multiplyByFive(7);
-            Console.WriteLine(result);
+            //Func<int, int> multiplyByFive = (int num) =>
+            //{
+            //    Console.WriteLine("tuoj dauginsiu is 5");
+            //    int result = num * 5;
+            //    return result;
+            //};
+            //int result = multiplyByFive(7);
+            //Console.WriteLine(result);
 
-            Func<int, int, int> multiply = (int num1, int num2) =>
-            {
-                Console.WriteLine($"tuoj dauginsiu is {num1} * {num2}");
-                return num1 * num2;
-            };
-            Console.WriteLine(multiply(3, 7));
+            //Func<int, int, int> multiply = (int num1, int num2) =>
+            //{
+            //    Console.WriteLine($"tuoj dauginsiu is {num1} * {num2}");
+            //    return num1 * num2;
+            //};
+            //Console.WriteLine(multiply(3, 7));
 
-            Func<bool, string, DateTime, decimal> differentParameterTypesFunc =
-                (arg1, arg2, arg3) => 1.78m;
+            //Func<bool, string, DateTime, decimal> differentParameterTypesFunc =
+            //    (arg1, arg2, arg3) => 1.78m;
 
-            Console.WriteLine(differentParameterTypesFunc(false, "string kintamasis", DateTime.Now));
+            //Console.WriteLine(differentParameterTypesFunc(false, "string kintamasis", DateTime.Now));
 
-            Func<string> funcWithoutParameters = () => "returning from func without parameters";
-            Console.WriteLine(funcWithoutParameters());
+            //Func<string> funcWithoutParameters = () => "returning from func without parameters";
+            //Console.WriteLine(funcWithoutParameters());
 
-            Func<int, bool> isIntegerHigherThanTen = (number) => number > 10;
-            int numberToCheck = 4;
-            if (isIntegerHigherThanTen(numberToCheck))
-            {
-                Console.WriteLine("Skaicius didesnis nei 10");
-            }
-            else
-            {
-                Console.WriteLine("Skaicius nedidesnis nei 10");
-            }
+            //Func<int, bool> isIntegerHigherThanTen = (number) => number > 10;
+            //int numberToCheck = 4;
+            //if (isIntegerHigherThanTen(numberToCheck))
+            //{
+            //    Console.WriteLine("Skaicius didesnis nei 10");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Skaicius nedidesnis nei 10");
+            //}
 
-            Func<int, int, int> filterNumber =
-                (number1, number2) => number1 > number2 ? number1 : number2;
+            //Func<int, int, int> filterNumber =
+            //    (number1, number2) => number1 > number2 ? number1 : number2;
 
 
+            Func<string, string> selector = (str) => str.ToUpper();
+            string[] words = new string[] { "orange", "apple", "Article", "elephant" };
+
+            //List<string> upperWords = new List<string>();
+            //foreach (string word in words)
+            //{
+            //    upperWords.Add(selector(word));
+            //}
+
+            var upperWordsWithLinq = words.Select((word) => word.ToUpper());    // Func<string, string>
+            var wordsLengths = words.Select((word) => word.Length);             // Func<string, int>
+            var areWordsLongerThan5 = words.Select((word) => word.Length > 5);  // Func<string, bool>
+
+            var wordsLongerThan5 = words.Where(str => str.Length > 5);
+
+            var orderedWords = words.OrderBy(word => word);
+            var orderedWordsByLength = words.OrderBy(word => word.Length);
+
+            // Grazint zodi pagal ilgi, arba ismes klaida jei keli zodziai tokio ilgio,
+            // arba grazins null jei nei vienas nera tokio ilgio
+            string singleOrDefaultWordWith8Symbols = words.SingleOrDefault(word => word.Length == 8);
+            // Tas pats kas SingleOrDefault, bet bus klaida jei nerado tokio ilgio zodzio
+            string singleWordWith8Symbols = words.Single(word => word.Length == 8);
+
+            var wordsCount = words.Count();
+            var wordsCountLongerThan5 = words.Count(word => word.Length > 5);
+
+            // ForEach veikia tik Listams taigi masyva paverciame i List<string> su ToList() metodu
+            // ir ForEach yra voidinis
+            words.ToList().ForEach(word => Console.WriteLine(word));
         }
     }
 }
